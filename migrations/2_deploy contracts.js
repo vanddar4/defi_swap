7f72dd40b769bd62f0c7715f149280b98d9cd9fa
vanddar4 <vanddar4@protonmail.com>
2021-02-04 00:59:10 -0500

    adding tokenFarm contract

--- migrations/2_deploy contracts.js
+++ migrations/2_deploy contracts.js
@@ -1,5 +1,25 @@
-const TokenFarm = artifacts.require('Migrations')
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
+const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+  //deplot Mocck DAI Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  //deploy Dapp Token
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+  //deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  //transfer all tokens to Tokenfarm (1 million)
+  await dappToken.transfer(tokenFarm.address, '100000000000000000000000')
+
+  //transfer 10 Dai Tokens to investor
+  await daiToken.transfer(accounts[1], '10000000000000000000')
+
+
 }
